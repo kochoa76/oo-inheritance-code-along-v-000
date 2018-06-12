9cc2b884e2d0f1acb10f67c0075bc69f4743f4e0
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-12 00:44:13 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,11 +1,4 @@
 require_relative "./vehicle.rb"
-class Car
-attr_accessor :wheel_size, :wheel_number 
-
-def initialize(wheel_size, wheel_number)
-  @wheel_size = wheel_size
-  @wheel_number = wheel_number
-end
-
-
+class Car < Vehicle
+ 
 end
